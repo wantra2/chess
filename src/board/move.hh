cf03ff8913595d88506c3c9ac867808cc90e2405
Esteban Martin <esteban.martin@epita.fr>
2019-05-21 17:01:03 +0200

    [MOVE] Added doc

--- src/board/move.hh
+++ src/board/move.hh
@@ -7,11 +7,36 @@
 
 namespace move
 {
+    /*
+      A Move is an 16 bits int ordered as :
+      bits 0 - 5   Move dst
+      bits 6 - 11  Move src
+      bits 12 - 13 Move flag: 1 -> capture, 2 -> castling, 3 -> en passant
+      bits 14 - 16 Promotion to the type piece_type
+     */
     using Move = uint16_t;
+
+    /*
+      These two functions convert a Move (int16) to a format more suitable for
+      the class Position
+     */
+    board::Rank int_to_rank(int i);
+    board::File int_to_file(int i);
+
+    /*
+      Returns a Move's source/destination formated as a Position
+     */
     board::Position move_src(Move move);
     board::Position move_dst(Move move);
+
+    /*
+      Move flags handler
+     */
     bool is_capture(Move move);
     bool is_castling(Move move);
+    bool is_passant(Move move);
+
     bool is_promotion(Move move);
+
     board::piece_type promotion_type(Move move);
 }
