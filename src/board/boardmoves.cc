b2f01c1398e496bec33d20bc291cb9d921d3150a
Denis Berenger <denis.berenger@epita.fr>
2019-05-25 02:00:19 +0200

    [MISC] fix compil error due to recent changes

--- src/board/boardmoves.cc
+++ src/board/boardmoves.cc
@@ -4,37 +4,37 @@ namespace board
 {
     void Board::remove_piece(square position, piece_type type, Color color)
     {
-        get_bitboard(type) ^= (0x1 << position);
-        get_bitboard(color) ^= (0x1 << position);
+        bitboards_[type] ^= (0x1 << position);
+        bitboards_[(int)color] ^= (0x1 << position);
     }
 
     void Board::remove_piece(File file, Rank rank, piece_type type, Color color)
     {
-        get_bitboard(type) ^= (0x1 << to_square((int)rank, (int)file));
-        get_bitboard(color) ^= (0x1 << to_square((int)rank, (int)file));
+        bitboards_[type] ^= (0x1 << to_square((int)rank, (int)file));
+        bitboards_[(int)color] ^= (0x1 << to_square((int)rank, (int)file));
     }
 
     void Board::remove_piece(const bitboard& piece, piece_type type, Color color)
     {
-        get_bitboard(type) ^= piece;
-        get_bitboard(color) ^= piece;
+        bitboards_[type] ^= piece;
+        bitboards_[(int)color] ^= piece;
     }
 
     void Board::add_piece(square position, piece_type type, Color color)
     {
-        get_bitboard(type) |= (0x1 << position);
-        get_bitboard(color) |= (0x1 << position);
+        bitboards_[type] |= (0x1 << position);
+        bitboards_[(int)color] |= (0x1 << position);
     }
 
     void Board::add_piece(File file, Rank rank, piece_type type, Color color)
     {
-        get_bitboard(type) |= (0x1 << to_square((int)rank, (int)file));
-        get_bitboard(color) |= (0x1 << to_square((int)rank, (int)file));
+        bitboards_[type] |= (0x1 << to_square((int)rank, (int)file));
+        bitboards_[(int)color] |= (0x1 << to_square((int)rank, (int)file));
     }
 
     void Board::add_piece(const bitboard& piece, piece_type type, Color color)
     {
-        get_bitboard(type) |= piece;
-        get_bitboard(color) |= piece;
+        bitboards_[type] |= piece;
+        bitboards_[(int)color] |= piece;
     }
 }
