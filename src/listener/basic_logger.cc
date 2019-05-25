8032a709c954d73a697d591d5c16fa73b0c15fa6
Esteban Martin <esteban.martin@epita.fr>
2019-05-25 13:12:59 +0200

    [LISTENER] Corrected the majority of output logs

--- src/listener/basic_logger.cc
+++ src/listener/basic_logger.cc
@@ -8,23 +8,22 @@
 namespace listener
 {
     void Basic_logger::register_board(const board::ChessboardInterface& board_interface)
-    {
-        std::cout << "hocus pocus, prepare your anus\n";
-    }
+    {}
     void Basic_logger::on_game_finished()
     {
-        std::cout << "Game finished\n";
+        std::cout << "[game_finished]\t\n";
     }
     void Basic_logger::on_piece_moved(const board::PieceType piece,
                                         const board::Position& from,
                                         const board::Position& to)
     {
-        std::cout << "Piece moved\n";
+        std::cout << "[move]\tboard::PieceType: " << (int)piece << '\n';
     }
     void Basic_logger::on_piece_taken(const board::PieceType piece,
                                         const board::Position& at)
     {
-        std::cout << "Piece taken\n";
+        std::cout << "[piece_taken]\tpiece: " << (int)piece << " file: "
+                  << (int)at.file_get() << " rank: " << (int)at.rank_get() << "\n";
     }
     void Basic_logger::on_piece_promoted(const board::PieceType piece,
                                         const board::Position& at)
@@ -33,32 +32,24 @@ namespace listener
     }
     void Basic_logger::on_kingside_castling(const board::Color color)
     {
-        std::cout << "Castling kingside\n";
+        std::cout << "[kingside castling]\tcolor: " << (int)color << '\n';
     }
     void Basic_logger::on_queenside_castling(const board::Color color)
     {
-        std::cout << "Castling queenside\n";
+        std::cout << "[queenside castling]\tcolor: " << (int)color << '\n';
     }
     void Basic_logger::on_player_check(const board::Color color)
     {
-        std::cout << "Check\n";
+        std::cout << "[check]\t: color: " << (int)color << '\n';
     }
     void Basic_logger::on_player_mat(const board::Color color)
     {
-        std::cout << "Checkmate\n";
-    }
-    void Basic_logger::on_player_pat(const board::Color color)
-    {
-        std::cout << "Stalemate\n";
+        std::cout << "[mat]\t: color: " << (int)color << '\n';
     }
     void Basic_logger::on_player_disqualified(const board::Color color)
     {
-        std::cout << "Disqualification\n";
+        std::cout << "[disqualified]\t: color: " << (int)color << '\n';
     }
     void Basic_logger::on_draw()
-    {
-        std::cout << "Draw\n";
-    }
+    {}
 }
-
-
