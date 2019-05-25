49c0809be5a8703973002ea474655daef76ed65f
Denis Berenger <denis.berenger@epita.fr>
2019-05-25 03:58:48 +0200

    [MOVEGEN] sliding pieces partly working

--- tests/movegen_slidin_Test.cc
+++ tests/movegen_slidin_Test.cc
@@ -6,12 +6,20 @@
 #include "board/color.hh"
 
 
-TEST(movegen_sliding, starting_position_queen)
+TEST(movegen_sliding, starting_position_queens_no_blockers)
 {
     board::Board boardd;
     std::vector<move::Move> movelist;
     boardd.gen_queen_bishop_moves(movelist, boardd.bitboards_[board::QUEEN], 0ull, 0xffffffffffffffff);
     boardd.gen_queen_rook_moves(movelist, boardd.bitboards_[board::QUEEN], 0ull, 0xffffffffffffffff);
+    ASSERT_EQ(movelist.size(), 42);
+}
+
+TEST(movegen_sliding, white_rooks_starting_pos_)
+{
+    board::Board boardd;
+    std::vector<move::Move> movelist;
+    boardd.gen_queen_rook_moves(movelist, boardd.bitboards_[board::ROOK]&boardd.bitboards_[0], 0x000000000000ff7e, 0x818181818181817e);
     ASSERT_EQ(movelist.size(), 0);
 }
 
