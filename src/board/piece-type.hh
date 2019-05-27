e54137acb1894b5c887f77a4471478dba6dd5054
Mamoune El Habbari <mamoune.el-habbari@epita.fr>
2019-05-27 17:49:32 +0200

    [PIECE_TYPE] added enum of piece types with colors

--- src/board/piece-type.hh
+++ src/board/piece-type.hh
@@ -31,6 +31,23 @@ namespace board
         KING
     };
 
+    enum piece_type_with_color : int
+    {
+        VOID,
+        WHITE_PAWN,
+        WHITE_KNIGHT,
+        WHITE_BISHOP,
+        WHITE_ROOK,
+        WHITE_QUEEN,
+        WHITE_KING,
+        BLACK_PAWN,
+        BLACK_KNIGHT,
+        BLACK_BISHOP,
+        BLACK_ROOK,
+        BLACK_QUEEN,
+        BLACK_KING
+    };
+
     using opt_piecetype_t = std::optional<PieceType>;
 
     /* Can be useful to iterate over PieceTypes */
