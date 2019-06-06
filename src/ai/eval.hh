8d176b81f82caaa3877942660f4f73ca19b2e963
Denis Berenger <denis.berenger@epita.fr>
2019-06-06 19:15:42 +0200

    [EVAL] tweak eval values

--- src/ai/eval.hh
+++ src/ai/eval.hh
@@ -70,8 +70,8 @@ namespace ai
             }
             };
     constexpr int pawn_value = 100;
-    constexpr int knight_value = 300;
-    constexpr int bishop_value = 300;
+    constexpr int knight_value = 325;
+    constexpr int bishop_value = 350;
     constexpr int rook_value = 500;
     constexpr int queen_value = 900;
     constexpr int king_value = 1000000;
